74b6124aa586acd45b52c21703c82b0156f8b9d6
Jared Sanchez <jared.10@live.com>
2023-11-20 20:06:05 -0600

    Updated spring-boot app with Maven methodology

---
+++ src/main/java/com/ltp/quickflix/MovieController.java
@@ -0,0 +1,53 @@
+package com.ltp.quickflix;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Controller
+@RequestMapping("/movies")
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @Autowired
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public String listMovies(Model model) {
+        model.addAttribute("movies", movieService.getAllMovies());
+        model.addAttribute("newMovie", new Movie());
+        return "movies";
+    }
+
+    @PostMapping("/add")
+    public String addMovie(@ModelAttribute Movie newMovie) {
+        movieService.addMovie(newMovie);
+        return "redirect:/movies";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable Integer id, Model model) {
+        Movie movie = movieService.getMovieById(id);
+        model.addAttribute("movie", movie);
+        return "editMovie";
+    }
+
+    @PostMapping("/update")
+    public String updateMovie(@ModelAttribute Movie movie) {
+        movieService.updateMovie(movie);
+        return "redirect:/movies";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteMovie(@PathVariable Integer id) {
+        movieService.deleteMovie(id);
+        return "redirect:/movies";
+    }
+}
