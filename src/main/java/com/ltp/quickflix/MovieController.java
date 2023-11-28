d83b00e651567234a93c647ba97cfdb2525e8813
Jared Sanchez <jared.10@live.com>
2023-11-28 10:00:49 -0600

    Added javadoc comments.

--- src/main/java/com/ltp/quickflix/MovieController.java
+++ src/main/java/com/ltp/quickflix/MovieController.java
@@ -10,15 +10,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Controller
 @RequestMapping("/movies")
+/**
+ * Controller for handling movie related requests.
+ */
 public class MovieController {
 
     private final MovieService movieService;
 
+    /**
+     * Constructs a new MovieController with the given MovieService.
+     *
+     * @param movieService the MovieService to use
+     */
     @Autowired
     public MovieController(MovieService movieService) {
         this.movieService = movieService;
     }
 
+    /**
+     * Handles GET requests for "/movies".
+     *
+     * @param model the model to add attributes to
+     * @return the name of the view to render
+     */
     @GetMapping
     public String listMovies(Model model) {
         model.addAttribute("movies", movieService.getAllMovies());
@@ -26,12 +40,25 @@ public class MovieController {
         return "movies";
     }
 
+    /**
+     * Handles POST requests for "/movies/add".
+     *
+     * @param newMovie the new movie to add
+     * @return the redirect view name
+     */
     @PostMapping("/add")
     public String addMovie(@ModelAttribute Movie newMovie) {
         movieService.addMovie(newMovie);
         return "redirect:/movies";
     }
 
+    /**
+     * Handles GET requests for "/movies/edit/{id}".
+     *
+     * @param id the ID of the movie to edit
+     * @param model the model to add attributes to
+     * @return the name of the view to render
+     */
     @GetMapping("/edit/{id}")
     public String showEditForm(@PathVariable Integer id, Model model) {
         Movie movie = movieService.getMovieById(id);
@@ -39,12 +66,24 @@ public class MovieController {
         return "editMovie";
     }
 
+    /**
+     * Handles POST requests for "/movies/update".
+     *
+     * @param movie the movie to update
+     * @return the redirect view name
+     */
     @PostMapping("/update")
     public String updateMovie(@ModelAttribute Movie movie) {
         movieService.updateMovie(movie);
         return "redirect:/movies";
     }
 
+    /**
+     * Handles GET requests for "/movies/delete/{id}".
+     *
+     * @param id the ID of the movie to delete
+     * @return the redirect view name
+     */
     @GetMapping("/delete/{id}")
     public String deleteMovie(@PathVariable Integer id) {
         movieService.deleteMovie(id);
--- src/main/java/com/ltp/quickflix/MovieService.java
+++ src/main/java/com/ltp/quickflix/MovieService.java
@@ -7,35 +7,72 @@ import org.springframework.stereotype.Service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Service for managing movies.
+ */
 @Service
 public class MovieService {
     private final List<Movie> movies = new ArrayList<>();
 
+    /**
+     * Returns a list of all movies.
+     *
+     * @return a list of all movies
+     */
     public List<Movie> getAllMovies() {
         return new ArrayList<>(movies);
     }
 
+    /**
+     * Adds a movie to the list.
+     *
+     * @param movie the movie to add
+     * @throws IllegalArgumentException if the movie's rating is not between 1 and 5
+     */
     public void addMovie(Movie movie) {
         validateRating(movie);
         movies.add(movie);
     }
 
+    /**
+     * Updates a movie in the list.
+     *
+     * @param updatedMovie the updated movie
+     * @throws IllegalArgumentException if the movie's rating is not between 1 and 5
+     */
     public void updateMovie(Movie updatedMovie) {
         validateRating(updatedMovie);
         movies.removeIf(m -> m.getId().equals(updatedMovie.getId()));
         movies.add(updatedMovie);
     }
 
+    /**
+     * Deletes a movie from the list.
+     *
+     * @param movieId the ID of the movie to delete
+     */
     public void deleteMovie(Integer movieId) {
         movies.removeIf(m -> m.getId().equals(movieId));
     }
 
+    /**
+     * Validates the rating of a movie.
+     *
+     * @param movie the movie to validate
+     * @throws IllegalArgumentException if the movie's rating is not between 1 and 5
+     */
     private void validateRating(Movie movie) {
         if (movie.getRating() < 1 || movie.getRating() > 5) {
             throw new IllegalArgumentException("Rating must be between 1 and 5.");
         }
     }
 
+    /**
+     * Returns the movie with the given ID.
+     *
+     * @param movieId the ID of the movie to return
+     * @return the movie with the given ID, or null if no such movie exists
+     */
     public Movie getMovieById(Integer movieId) {
         return movies.stream().filter(m -> m.getId().equals(movieId)).findFirst().orElse(null);
     }
